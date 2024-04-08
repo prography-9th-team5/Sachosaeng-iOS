1401349dac44feb4c2d4e98aa27ac14c45cd9801
Jehyeon Lee <dasom8899981@gmail.com>
2024-04-08 19:40:24 +0900

    create: gitignore

---
+++ Sachosaeng/Sachosaeng/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sachosaeng
+//
+//  Created by LJh on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sachosaeng/Sachosaeng/SachosaengApp.swift
@@ -0,0 +1,17 @@
+//
+//  SachosaengApp.swift
+//  Sachosaeng
+//
+//  Created by LJh on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SachosaengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

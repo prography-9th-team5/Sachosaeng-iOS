a8e278db5c8cf9336ab6cfb74b57866a7f5643d4
Jehyeon Lee <dasom8899981@gmail.com>
2024-06-22 20:17:49 +0900

    feat: signInGoogle 메서드

--- Sachosaeng/Sachosaeng/View/UserOccupationView.swift
+++ Sachosaeng/Sachosaeng/View/UserOccupationView.swift
@@ -12,7 +12,6 @@ struct UserOccupationView: View {
     // MARK: - Properties
     @State private var selectedOccupations: [Bool] = Array(repeating: false, count: 4)
     @State var isSelected: Bool = false
-    
     // MARK: - Body
     var body: some View {
         NavigationStack {

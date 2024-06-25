99fadb67e84347be10e0bfee2303fc5826212ed9
Jehyeon Lee <dasom8899981@gmail.com>
2024-06-26 07:59:00 +0900

    feat: 탭바 커스텀
    problem: 탭바 커스텀을 했는데 safearea 쪽까지 안먹음

--- Sachosaeng/Sachosaeng/HomeView/MainView.swift
+++ Sachosaeng/Sachosaeng/HomeView/MainView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+enum TabItem {
+    case home
+    case setting
+}
+
 struct MainView: View {
+    @State var switchTab: TabItem = .home
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house.fill")
+        VStack {
+            // 상단 콘텐츠 영역
+            switch switchTab {
+                case .home:
+                    HomeView()
+                case .setting:
+                    SignView()
+            }
+            
+            Spacer() // 상단 콘텐츠를 위로 밀어 올리기 위한 Spacer
+            
+            // 하단 탭바 영역
+            HStack(spacing: 0) {
+                Spacer()
+                Button {
+                    switchTab = .home
+                } label: {
+                    Image(systemName: "house")
                 }
-        }           
-        .navigationBarBackButtonHidden(true)
+                
+                Spacer()
+
+                Button {
+                    switchTab = .setting
+                } label: {
+                    Image(systemName: "house")
+                }
+                Spacer()
+
+            }
+            .frame(height: 50)
+            .background(CustomColor.GrayScaleColor.gs4)
+            .cornerRadius(15, corners: [.topLeft, .topRight])
+            .ignoresSafeArea(edges: .bottom)
+            
+        }.navigationBarBackButtonHidden(true)
     }
 }
 

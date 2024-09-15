b13d7bd818db4c50a0827e5ad8a90382fa37c359
Jehyeon Lee <dasom8899981@gmail.com>
2024-09-15 17:17:02 +0900

    feat: 인증 스토어 생성

---
+++ Sachosaeng/Sachosaeng/ViewModel/VersionStore.swift
@@ -0,0 +1,16 @@
+//
+//  VersionStore.swift
+//  Sachosaeng
+//
+//  Created by LJh on 9/14/24.
+//
+
+import Foundation
+
+class VersionStore {
+    
+    
+    func searchVersion() {
+        
+    }
+}

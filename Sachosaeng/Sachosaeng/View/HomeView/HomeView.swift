2f75bc48f8959756905cdefdf492ce0cd0bf01ea
Jehyeon Lee <dasom8899981@gmail.com>
2025-01-02 22:16:21 +0900

    design: 인기투표 리스트 셀 디자인 변경
    - 기존 - 자 모양 -> 직사각형 코너 박스로 변경

--- Sachosaeng/Sachosaeng/View/HomeView/HomeView.swift
+++ Sachosaeng/Sachosaeng/View/HomeView/HomeView.swift
@@ -75,21 +75,33 @@ struct HomeView: View {
                                 }
                                 .padding(.horizontal, 20)
                                 
-                                VStack(spacing: 0) {
-                                    ForEach(Array(voteStore.hotVotes.votes.enumerated()), id: \.element) { index, vote in
-                                        Button {
-                                            if isTouchedVoteIndex == nil {
-                                                isTouchedVoteIndex = vote.voteId
-                                                path.append(PathType.voteDetail(vote.voteId))
+//                                VStack(spacing: 0) {
+//                                    
+//                                }
+//                                .padding(.bottom, 32)
+                                
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 0) {
+                                        ForEach(Array(voteStore.hotVotes.votes.enumerated()), id: \.element) { index, vote in
+                                            Button {
+                                                if isTouchedVoteIndex == nil {
+                                                    isTouchedVoteIndex = vote.voteId
+                                                    path.append(PathType.voteDetail(vote.voteId))
+                                                }
+                                            } label: {
+                                                // MARK: - 작업중
+
+                                                setHotVoteCell(vote: vote, index: index)
+                                                    .padding(.trailing, 8)
                                             }
-                                        } label: {
-                                            setHotVoteCell(vote: vote, index: index)
-                                        }
-                                        .onAppear {
-                                            isTouchedVoteIndex = nil
+                                            .onAppear {
+                                                isTouchedVoteIndex = nil
+                                            }
                                         }
                                     }
+                                    
                                 }
+                                .padding(.leading, 20)
                                 .padding(.bottom, 32)
                                 
                                 ForEach(voteStore.hotVotesInCategory) { hotVote in
@@ -432,60 +444,75 @@ extension HomeView {
     
     @ViewBuilder
     private func setHotVoteCell(vote: Vote, index: Int) -> some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 8)
-                .frame(height: 60)
-                .foregroundStyle(vote.isVoted ? CustomColor.GrayScaleColor.gs3 : CustomColor.GrayScaleColor.white)
-            HStack(spacing: 0) {
-                if vote.isVoted {
-                    Image("checkCircle_true")
-                        .circleImage(frame: 16)
-                        .padding(.leading, 16)
-                } else {
-                    Text("\(index + 1)")
-                        .font(.createFont(weight: .bold, size: 15))
-                        .foregroundStyle(CustomColor.GrayScaleColor.black)
-                        .padding(.leading, 16)
-                }
+//        ZStack {
+//            RoundedRectangle(cornerRadius: 8)
+//                .frame(height: 60)
+//                .foregroundStyle(vote.isVoted ? CustomColor.GrayScaleColor.gs3 : CustomColor.GrayScaleColor.white)
+//            HStack(spacing: 0) {
+//                if vote.isVoted {
+//                    Image("checkCircle_true")
+//                        .circleImage(frame: 16)
+//                        .padding(.leading, 16)
+//                } else {
+//                    Text("\(index + 1)")
+//                        .font(.createFont(weight: .bold, size: 15))
+//                        .foregroundStyle(CustomColor.GrayScaleColor.black)
+//                        .padding(.leading, 16)
+//                }
+                
                 VStack(alignment: .leading, spacing: 0) {
-                    HStack(spacing: 0) {
-                        Text(vote.title)
-                            .font(.createFont(weight: .bold, size: 15))
-                            .foregroundStyle(CustomColor.GrayScaleColor.black)
-                            .lineLimit(1)
-                        Spacer()
-                    }
-                    
-                    HStack(spacing : 0) {
-                        if let participantCount = vote.participantCount, participantCount > 10 {
-                            HStack(spacing: 0) {
+                    VStack(spacing: 0) {
+                        HStack(spacing: 0) {
+                            if let participantCount = vote.participantCount, participantCount > 10 {
                                 Text("\(participantCount)명 참여 중")
                                     .font(.createFont(weight: .medium, size: 12))
                                     .foregroundStyle(CustomColor.GrayScaleColor.gs6)
-                                Spacer()
+                                    
                             }
+                            Spacer()
+                        }
+                        
+                        
+                        HStack(spacing: 0) {
+                            Text(vote.title)
+                                .font(.createFont(weight: .semiBold, size: 15))
+                                .foregroundStyle(CustomColor.GrayScaleColor.black)
+                                .lineLimit(3)
+                                .multilineTextAlignment(.leading)
+                            Spacer()
                         }
                     }
-                }
-                .padding(.leading, 10)
-                ZStack {
-                    RoundedRectangle(cornerRadius: 4)
-                        .frame(width: 32, height: 32)
-                        .foregroundStyle(Color(hex: vote.category.backgroundColor))
-                    AsyncImage(url: URL(string: vote.category.iconUrl)) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 18, height: 18)
-                    } placeholder: {
-                        ProgressView()
-                    }
+                    .padding(.top, 20)
+                    .padding(.leading, 16)
                     
+                    Spacer()
+
+                    HStack(spacing: 0) {
+                        Spacer()
+                        AsyncImage(url: URL(string: vote.category.iconUrl)) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 32, height: 32)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .padding(16)
+                    }
                 }
-                .padding(.trailing, 16)
-            }
-        }
-        .padding(.horizontal, 20)
-        .padding(.bottom, 6)
+                .background(Color(hex: vote.category.backgroundColor))
+                .cornerRadius(8, corners: .allCorners)
+                .frame(width: 156, height: 176)
+
+//                ZStack {
+//                    RoundedRectangle(cornerRadius: 4)
+//                        .frame(width: 32, height: 32)
+//                        .foregroundStyle(Color(hex: vote.category.backgroundColor))
+                    
+                  
+//            }
+//        }
+//        .padding(.horizontal, 20)
+//        .padding(.bottom, 6)
     }
 }
